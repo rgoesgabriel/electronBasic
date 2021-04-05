6a0c235412b3e4ee5fa39ab8fbbcf22319cbe385
Gabriel Goes <gabriel.goes@championx.com>
2021-04-05 14:38:02 -0300

    Add1

---
+++ main.js
@@ -0,0 +1,34 @@
+const { app, BrowserWindow } = require('electron')
+
+let win;
+
+function createWindow () {
+    win = new BrowserWindow({
+        width: 1080,
+        height:720,
+        backgroundColor: '#ffffff',
+        icon: `file://${__dirname}/dist/assets/logo.png`
+    })
+
+    win.loadURL(`file://${__dirname}/dist/index.html`)
+
+    // win.webContents.openDevTools()
+
+    win.on('closed', function () {
+        win = null
+    })
+}
+
+app.on('ready', createWindow)
+
+app.on('window-all-closed', function() {
+    if(process.platform !== 'darwin') {
+        app.quit()
+    }
+})
+
+app.on('activate', function() {
+    if(win === null) {
+        createWindow()
+    }
+})
